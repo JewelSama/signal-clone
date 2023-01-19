0703fbf9c9984c3e3f9aef9cfad11877461ecd36
Jewel Osafile <osafilejewel@gmail.com>
2023-01-19 05:41:21 +0100

    navig

--- App.js
+++ App.js
@@ -19,10 +19,13 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={globalScreenOptions}>
+      <Stack.Navigator 
+      // initialRouteName='Home'
+      screenOptions={globalScreenOptions}
+      >
         <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
